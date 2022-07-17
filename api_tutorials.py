c7b0145d66678bfa5704a3d4fec549e6f947806b
danielAdama <adamadaniel321.com>
2022-07-17 18:25:30 +0100

    API practice with HTTP methods post, get, delete

--- api_tutorials.py
+++ api_tutorials.py
@@ -1,5 +1,7 @@
+from email import header
 import flask
 import requests
+import json
 
 # url = "https://jsonplaceholder.typicode.com/todos/1"
 # response = requests.get(url)
@@ -7,9 +9,26 @@ import requests
 # print(response.status_code)
 # print(response.headers["Content-Type"])
 
-url='https://jsonplaceholder.typicode.com/todos/'
-todo = {"userId": 1, "title": "Buy milk", "completed": False}
-response = requests.post(url=url, json=todo)
-print(response.json())
-print(response.status_code)
+## Post a data 1st process
+# url='https://jsonplaceholder.typicode.com/todos/'
+# todo = {"userId": 1, "title": "Buy milk", "completed": False}
+# header = {"Content-Type":"application/json"}
+# response = requests.post(url=url, data=json.dumps(todo), headers=header)
+# print(response.json())
+# print(response.status_code)
+
+## Post a data 2nd process
+# url='https://jsonplaceholder.typicode.com/todos/'
+# todo = {'userId': 1, 'title': 'Wash car', 'completed': True, 'id': 10}
+# header = {"Content-Type":"application/json"}
+# response = requests.post(url=url, data=json.dumps(todo), headers=header)
+# print(response.json())
+# print(response.status_code)
+
+## Patch to modify an entry
+# url='https://jsonplaceholder.typicode.com/todos/10'
+# todo = {"title": "Mow lawn"}
+# response = requests.patch(url=url, json=todo)
+# print(response.json())
+# print(response.status_code)
 
